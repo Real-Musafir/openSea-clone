c310df39664e7dc0b3a31605684ce87a0e49ba98
Real-Musafir <alshahadath@gmail.com>
2022-04-06 21:48:14 +0600

    load nft's in index.js for frontend

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; //prevent re-entrancy attacks
+ //prevents re-entrancy attacks
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
-    Counters.Counter private _itemIds;
-    Counters.Counter private _itemsSold; //Total number of item sold
+    Counters.Counter private _itemIds; //total number of items ever created
+    Counters.Counter private _itemsSold; //total number of items sold
 
-    address payable owner; // owner of the smart contract
-
-    //people have to pay their NFT on this marketplace
+    address payable owner; //owner of the smart contract
+    //people have to pay to puy their NFT on this marketplace
     uint256 listingPrice = 0.025 ether;
 
     constructor(){
@@ -23,36 +23,36 @@ contract NFTMarket is ReentrancyGuard {
         uint itemId;
         address nftContract;
         uint256 tokenId;
-        address payable seller;
-        address payable owner;
-        uint price;
-        bool sold; 
+        address payable seller; //person selling the nft
+        address payable owner; //owner of the nft
+        uint256 price;
+        bool sold;
     }
 
     //a way to access values of the MarketItem struct above by passing an integer ID
     mapping(uint256 => MarketItem) private idMarketItem;
 
-    //log message (when item is sold)
-    event MarketItemCreated(
-       uint indexed itemId,
+    //log message (when Item is sold)
+    event MarketItemCreated (
+        uint indexed itemId,
         address indexed nftContract,
         uint256 indexed tokenId,
         address  seller,
         address  owner,
-        uint price,
+        uint256 price,
         bool sold
     );
 
-    /// @notice function to get listing price
-    function getListingPrice() public view returns(uint256){
+    /// @notice function to get listingprice
+    function getListingPrice() public view returns (uint256){
         return listingPrice;
     }
 
     function setListingPrice(uint _price) public returns(uint) {
-       if(msg.sender == address(this)){
-            listingPrice = _price;
-       }
-        return listingPrice;
+         if(msg.sender == address(this) ){
+             listingPrice = _price;
+         }
+         return listingPrice;
     }
 
     /// @notice function to create market item
@@ -60,140 +60,148 @@ contract NFTMarket is ReentrancyGuard {
         address nftContract,
         uint256 tokenId,
         uint256 price) public payable nonReentrant{
-            require(price>0, "Price must be above zero");
-            require(msg.value == listingPrice, "Price Must be equal to the listing price");
-            
-            _itemIds.increment();
-            uint256 itemId = _itemIds.current();
+         require(price > 0, "Price must be above zero");
+         require(msg.value == listingPrice, "Price must be equal to listing price");
 
-           idMarketItem[itemId] = MarketItem(
-               itemId,
-               nftContract,
-               tokenId,
-               payable(msg.sender), //address of the seller putting the nft up for sale
-               payable(address(0)), // no owner yet (set owner to empty address)
-               price,
-               false
-           );
+         _itemIds.increment(); //add 1 to the total number of items ever created
+         uint256 itemId = _itemIds.current();
 
-           // transfer ownership of the nft to the contract itself
-           IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId); //in safeTransferFrom(from, to, tokenId)
+         idMarketItem[itemId] = MarketItem(
+             itemId,
+             nftContract,
+             tokenId,
+             payable(msg.sender), //address of the seller putting the nft up for sale
+             payable(address(0)), //no owner yet (set owner to empty address)
+             price,
+             false
+         );
 
-           //Log this transaction
-           emit MarketItemCreated(
-               itemId,
-               nftContract,
-               tokenId,
-               msg.sender,
-               address(0),
-               price,
-               false);
+            //transfer ownership of the nft to the contract itself
+            IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+            //log this transaction
+            emit MarketItemCreated(
+                itemId,
+             nftContract,
+             tokenId,
+             msg.sender,
+             address(0),
+             price,
+             false);
 
         }
 
 
         /// @notice function to create a sale
-        function createMarketSale(address nftContract, uint256 itemId) public payable nonReentrant{
-            uint price = idMarketItem[itemId].price;
-            uint tokenId =  idMarketItem[itemId].tokenId;
+        function createMarketSale(
+            address nftContract,
+            uint256 itemId
+            ) public payable nonReentrant{
+                uint price = idMarketItem[itemId].price;
+                uint tokenId = idMarketItem[itemId].tokenId;
 
-            require(msg.value == price, "Please submit the asking price in order to complete purchase");
+                require(msg.value == price, "Please submit the asking price in order to complete purchase");
 
-            //pay the seller the amount
-            idMarketItem[itemId].seller.transfer(msg.value);
+           //pay the seller the amount
+           idMarketItem[itemId].seller.transfer(msg.value);
 
-             // transfer ownership of the nft from the contract itself to the buyer
-           IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId); //in safeTransferFrom(from, to, tokenId)
+             //transfer ownership of the nft from the contract itself to the buyer
+            IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
 
-           idMarketItem[itemId].owner = payable(msg.sender); // mark buyer as new owner
-           idMarketItem[itemId].sold = true; // mark that is has been sold
-           _itemsSold.increment(); //increment the total number of Items sold by 1
-           payable(owner).transfer(listingPrice); //pay owner of contract the listing price
+            idMarketItem[itemId].owner = payable(msg.sender); //mark buyer as new owner
+            idMarketItem[itemId].sold = true; //mark that it has been sold
+            _itemsSold.increment(); //increment the total number of Items sold by 1
+            payable(owner).transfer(listingPrice); //pay owner of contract the listing price
         }
+
 
         /// @notice total number of items unsold on our platform
-        function fetchMarketItems() public view returns(MarketItem[] memory) {
-            uint itemCount = _itemIds.current(); // total number of item ever created in our platform
-
+        function fetchMarketItems() public view returns (MarketItem[] memory){
+            uint itemCount = _itemIds.current(); //total number of items ever created
+            //total number of items that are unsold = total items ever created - total items ever sold
             uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
-            
             uint currentIndex = 0;
 
-            MarketItem[] memory items = new MarketItem[](unsoldItemCount);
-            //loop throug all items ever created
-            for(uint i=0; i<itemCount; i++){
-                // check if the item has not been sold
-                // by checking if the owner field if emtpy
+            MarketItem[] memory items =  new MarketItem[](unsoldItemCount);
+
+            //loop through all items ever created
+            for(uint i = 0; i < itemCount; i++){
+
+                //get only unsold item
+                //check if the item has not been sold
+                //by checking if the owner field is empty
                 if(idMarketItem[i+1].owner == address(0)){
-                    // Yes this item has never been sold
-                    uint currentId = idMarketItem[i+1].itemId;
+                    //yes, this item has never been sold
+                    uint currentId = idMarketItem[i + 1].itemId;
                     MarketItem storage currentItem = idMarketItem[currentId];
                     items[currentIndex] = currentItem;
-                    currentIndex +=1;
+                    currentIndex += 1;
+
                 }
             }
-
-            return items; // return array of all unsold items
+            return items; //return array of all unsold items
         }
 
-        /// @notice fetch list of NFTS owned bought by this user
-        function fetchMyNFTs() public view returns(MarketItem[] memory){
-            // get total number of items ever created
+        /// @notice fetch list of NFTS owned/bought by this user
+        function fetchMyNFTs() public view returns (MarketItem[] memory){
+            //get total number of items ever created
             uint totalItemCount = _itemIds.current();
 
             uint itemCount = 0;
             uint currentIndex = 0;
 
-            for(uint i = 0; i<=totalItemCount; i++){
-                if(idMarketItem[i+1].owner==msg.sender){
-                    itemCount +=1;
+
+            for(uint i = 0; i < totalItemCount; i++){
+                //get only the items that this user has bought/is the owner
+                if(idMarketItem[i+1].owner == msg.sender){
+                    itemCount += 1; //total length
                 }
             }
 
             MarketItem[] memory items = new MarketItem[](itemCount);
-
-            for(uint i =0; i<totalItemCount; i++){
-                if(idMarketItem[i+1].owner==msg.sender){
-                    uint currentId = idMarketItem[i+1].itemId;
-                    MarketItem storage currentItem = idMarketItem[currentId];
-                    items[currentIndex] = currentItem;
-                    currentIndex +=1;
-
-                }
+            for(uint i = 0; i < totalItemCount; i++){
+               if(idMarketItem[i+1].owner == msg.sender){
+                   uint currentId = idMarketItem[i+1].itemId;
+                   MarketItem storage currentItem = idMarketItem[currentId];
+                   items[currentIndex] = currentItem;
+                   currentIndex += 1;
+               }
             }
-
             return items;
 
         }
 
-         /// @notice fetch list of NFTS created by this user
-        function fetchItemsCreated() public view returns(MarketItem[] memory){
-            // get total number of items ever created
+
+         /// @notice fetch list of NFTS owned/bought by this user
+        function fetchItemsCreated() public view returns (MarketItem[] memory){
+            //get total number of items ever created
             uint totalItemCount = _itemIds.current();
 
             uint itemCount = 0;
             uint currentIndex = 0;
 
-            for(uint i = 0; i<=totalItemCount; i++){
-                if(idMarketItem[i+1].seller==msg.sender){
-                    itemCount +=1;
+
+            for(uint i = 0; i < totalItemCount; i++){
+                //get only the items that this user has bought/is the owner
+                if(idMarketItem[i+1].seller == msg.sender){
+                    itemCount += 1; //total length
                 }
             }
 
             MarketItem[] memory items = new MarketItem[](itemCount);
-
-            for(uint i =0; i<totalItemCount; i++){
-                if(idMarketItem[i+1].seller==msg.sender){
-                    uint currentId = idMarketItem[i+1].itemId;
-                    MarketItem storage currentItem = idMarketItem[currentId];
-                    items[currentIndex] = currentItem;
-                    currentIndex +=1;
-
-                }
+            for(uint i = 0; i < totalItemCount; i++){
+               if(idMarketItem[i+1].seller == msg.sender){
+                   uint currentId = idMarketItem[i+1].itemId;
+                   MarketItem storage currentItem = idMarketItem[currentId];
+                   items[currentIndex] = currentItem;
+                   currentIndex += 1;
+               }
             }
-
             return items;
 
         }
+
+
+
 
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
-import axios from "axios";
 import { ethers } from "ethers";
-import Web3Modal from "Web3Modal";
 import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
-
+import axios from "axios";
+import Web3Modal from "web3modal";
 import { nftaddress, nftmarketaddress } from "../config";
-
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import Image from "next/image";
 
 export default function Home() {
   const [nfts, setNfts] = useState([]);
   const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
 
   async function loadNFTs() {
     const provider = new ethers.providers.JsonRpcProvider();
@@ -23,25 +25,23 @@ export default function Home() {
       provider
     );
 
-    //returns an array of unsold market items
+    //return an array of unsold market items
     const data = await marketContract.fetchMarketItems();
 
     const items = await Promise.all(
       data.map(async (i) => {
         const tokenUri = await tokenContract.tokenURI(i.tokenId);
-
         const meta = await axios.get(tokenUri);
         let price = ethers.utils.formatUnits(i.price.toString(), "ether");
         let item = {
           price,
-          tokenId: i.tokentId.toNumber(),
+          tokenId: i.tokenId.toNumber(),
           seller: i.seller,
           owner: i.owner,
           image: meta.data.image,
           name: meta.data.name,
           description: meta.data.description,
         };
-
         return item;
       })
     );
@@ -50,17 +50,74 @@ export default function Home() {
     setLoadingState("loaded");
   }
 
-  if (loadingState === "loaded" && !nfts.length) {
-    return <h1 className="px-20 py-10 text-3xt">No items in market place</h1>;
+  async function buyNFT(nft) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    //sign the transaction
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+
+    //set the price
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    //make the sale
+    const transaction = await contract.createMarketSale(
+      nftaddress,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
+    await transaction.wait();
+
+    loadNFTs();
   }
 
-  useEffect(() => {
-    loadNFTs();
-  }, []);
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="px-20 py-10 text-3xl">No items in market place</h1>;
 
   return (
-    <div className={styles.container}>
-      <h1>Welcome to home!</h1>
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <Image
+                src={nft.image}
+                alt="Picture of the author"
+                width={500}
+                height={500}
+                // blurDataURL="data:..." automatically provided
+                // placeholder="blur" // Optional blur-up while loading
+              />
+              <div className="p-4">
+                <p
+                  style={{ height: "64px" }}
+                  className="text-2xl font-semibold"
+                >
+                  {nft.name}
+                </p>
+                <div style={{ height: "70px", overflow: "hidden" }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl mb-4 font-bold text-white">
+                  {nft.price} ETH
+                </p>
+                <button
+                  className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                  onClick={() => buyNFT(nft)}
+                >
+                  Buy NFT
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
